98521a5244296e2bc2364b869cb3e11745fb4c1f
Agbugba David C <57929456+Davidruph@users.noreply.github.com>
2023-11-14 20:55:33 +0100

    Initial commit

---
+++ src/theme/ThemeProvider.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/prop-types */
+import { useState, useEffect } from "react";
+import ThemeContext from "./ThemeContext";
+
+const ThemeProvider = ({ children }) => {
+  const [isDarkMode, setDarkMode] = useState(true);
+
+  useEffect(() => {
+    // const darkStyle = document.getElementById("dark-theme-style");
+    // const handleStylesReady = () => {
+    //   darkStyle.removeEventListener("load", handleStylesReady);
+    // };
+    // darkStyle.addEventListener("load", handleStylesReady);
+    // darkStyle.removeAttribute("disabled");
+  }, []);
+
+  return (
+    <ThemeContext.Provider
+      value={{ isDarkMode, toggleTheme: () => setDarkMode(!isDarkMode) }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeProvider;
